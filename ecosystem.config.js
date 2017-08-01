4399fc9cc6eafe35e2dcda4e7f4a4cd6e878b235
David <dav2022@gmail.com>
2017-08-01 22:25:53 +0200

    pm2 s

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
             ref: 'origin/master',
             repo: 'https://github.com/mrphilips/APIFTP.git',
             path: '/home/ubuntu/tutorial-2',
-            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+            'post-deploy': 'sudo npm install && pm2 startOrRestart ecosystem.config.js'
         }
     }
 }
