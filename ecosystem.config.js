183e80156a9094f717b0749d2a3cd0644799464c
David <dav2022@gmail.com>
2017-08-01 22:14:50 +0200

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+        name: 'tutorial-2',
+        script: './bin/WWW'
+    }],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-18-220-131-37.us-east-2.compute.amazonaws.com',
+            key: '~/.ssh/tutorial.pem',
+            ref: 'origin/master',
+            repo: 'git@github.com:mrphilips/APIFTP.git',
+            path: '/home/ubuntu/tutorial-2',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+}
