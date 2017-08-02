1cd6cbc5587a73271fc7bd5a932c954f43275af8
David <dav2022@gmail.com>
2017-08-02 20:39:00 +0200

    test pm2

--- bin/index.js
+++ bin/index.js
@@ -12,11 +12,11 @@ app.set('port', process.env.PORT || 3000);
     cert: fs.readFileSync('ssl/ca.crt')
 };*/
 
-var sslOptions = {
+/*var sslOptions = {
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cer')
-};
+};*/
 
-var server = https.createServer(sslOptions, app).listen(3000, function() {
+var server = http.createServer(app).listen(3000, function() {
   console.log('Express server listening on port ' + server.address().port);
 });
