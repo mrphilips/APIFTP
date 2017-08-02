ab6bc0fcea69676b755907e4d2607ff8bbb01956
David <dav2022@gmail.com>
2017-08-02 20:39:42 +0200

    update eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-18-220-131-37.us-east-2.compute.amazonaws.com',
+            host: 'ec2-13-59-42-82.us-east-2.compute.amazonaws.com',
             key: '~/.ssh/tutorial.pem',
             ref: 'origin/master',
             repo: 'https://github.com/mrphilips/APIFTP.git',
