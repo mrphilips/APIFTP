093aa7b1ea03fa30cdb46afb96114cd12029dd8e
David <dav2022@gmail.com>
2017-08-16 23:14:28 +0200

    oms deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     apps: [{
         name: 'tutorial-2',
         script: './bin/index.js'
@@ -11,6 +11,24 @@ module.exports = {
             ref: 'origin/master',
             repo: 'https://github.com/mrphilips/APIFTP.git',
             path: '/home/ubuntu/tutorial-2',
+            'post-deploy': 'sudo npm install && sudo pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+}*/
+
+module.exports = {
+    apps: [{
+        name: 'omschr',
+        script: './bin/index.js'
+    }],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-18-220-223-224.us-east-2.compute.amazonaws.com',
+            key: '~/.ssh/devoms.pem',
+            ref: 'origin/master',
+            repo: 'https://github.com/mrphilips/APIFTP.git',
+            path: '/home/ubuntu/server',
             'post-deploy': 'sudo npm install && sudo pm2 startOrRestart ecosystem.config.js'
         }
     }
